24ab8a0fe6c58e3981991703aeb5f54348d6522f
Nicholas Gilbert <nickgilbert2@gmail.com>
2020-04-16 21:13:57 -0700

    Adds a navbar that actually does link things!

--- src/Main.elm
+++ src/Main.elm
@@ -7,6 +7,7 @@ import Html.Attributes exposing (..)
 import Url
 import Bootstrap.CDN as CDN
 import Bootstrap.Grid as Grid
+import Bootstrap.Navbar as Navbar
 
 main : Program () Model Msg
 main =
@@ -20,21 +21,30 @@ main =
         }
 
 type alias Model =
-    { key : Nav.Key
+    { navbarState : Navbar.State 
     , url : Url.Url
+    , key : Nav.Key
     }
 
+ 
 init : () -> Url.Url -> Nav.Key -> ( Model, Cmd Msg )
-init flags url key =
-    (Model key url, Cmd.none)
+init toMsg url key =
+    let
+        (navbarState, navbarCmd)
+            = Navbar.initialState NavbarMsg
+    in
+        (Model navbarState url key, navbarCmd )
 
 type Msg
-    = LinkClicked Browser.UrlRequest
+    = NavbarMsg Navbar.State
+    | LinkClicked Browser.UrlRequest
     | UrlChanged Url.Url
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
+        NavbarMsg state ->
+            ( { model | navbarState = state }, Cmd.none)
         LinkClicked urlRequest ->
             case urlRequest of
                 Browser.Internal url ->
@@ -48,7 +58,7 @@ update msg model =
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
-    Sub.none
+    Navbar.subscriptions model.navbarState NavbarMsg
 
 view : Model -> Browser.Document Msg
 view model =
@@ -56,33 +66,25 @@ view model =
     , body =
         [ Grid.container []
             [ CDN.stylesheet
+            , menu model
             , Grid.row []
                 [ Grid.col []
                     [ b [] [ text (Url.toString model.url) ] ]
-                ]
-            , Grid.row []
-                [ Grid.col []
-                    [ viewLink "/home"
-                    , viewLink "/hello" 
-                    ]
                 ]
             ]
         ]
     }
 
---    { title = "URL Interceptor"
---    , body =
---        [ text "The current URL is: "
---        , b [] [ text (Url.toString model.url) ]
---        , ul []
---            [ viewLink "/home"
---            , viewLink "/profile"
---            , viewLink "/reviews/the-century-of-the-self"
---            , viewLink "/reviews/public-opinion"
---            , viewLink "/reviews/shah-of-shahs"
---            ]
---        ]
---    }
+menu : Model -> Html Msg
+menu model =
+    Navbar.config NavbarMsg
+        |> Navbar.withAnimation
+        |> Navbar.brand [ href "#" ] [ text "Home" ]
+        |> Navbar.items
+            [ Navbar.itemLink [ href "/foo" ] [text "foo" ]
+            , Navbar.itemLink [ href "/bar" ] [text "bar" ]
+            ]
+        |> Navbar.view model.navbarState
 
 viewLink : String -> Html msg
 viewLink path =
